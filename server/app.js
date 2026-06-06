705343980af22cb3c5b6d2cae702fcf027bce279
kannan <grsankar96@gmail.com>
2026-06-06 11:49:04 +0530

    fix the issue faced in cloudinary connection

--- server/app.js
+++ server/app.js
@@ -96,8 +96,8 @@ app.use('/api/', limiter);
 connectDB();
 
 
-app.use(express.json({ limit: '50kb' }));
-app.use(express.urlencoded({ extended: true, limit: '50kb' }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 
 
